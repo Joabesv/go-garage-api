c7bfc55bea45840933ead44b02b0085045023087
Joabesv <joabevarjao123@gmail.com>
2023-06-27 13:04:11 -0300

    write error middleware

---
+++ business/web/v1/mid/errors.go
@@ -0,0 +1,51 @@
+package mid
+
+import (
+	"context"
+	"net/http"
+
+	v1Web "github.com/ardanlabs/service/business/web/v1"
+	"github.com/ardanlabs/service/foundation/web"
+	"go.uber.org/zap"
+)
+
+func Error(log *zap.SugaredLogger) web.Middleware {
+	m := func(handler web.Handler) web.Handler {
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+			if err := handler(ctx, w, r); err != nil {
+				log.Errorw("ERROR", "trace_id", web.GetTraceID(ctx), "message", err)
+				var er v1Web.ErrorResponse
+				var status int
+
+				switch {
+				case v1Web.IsRequestError(err):
+					reqErr := v1Web.GetRequestError(err)
+					er = v1Web.ErrorResponse{
+						Error: reqErr.Error(),
+					}
+					status = reqErr.Status
+
+				default:
+					er = v1Web.ErrorResponse{
+						Error: http.StatusText(http.StatusInternalServerError),
+					}
+					status = http.StatusInternalServerError
+				}
+
+				if err := web.Respond(ctx, w, er, status); err != nil {
+					return err
+				}
+
+				if web.IsShutdown(err) {
+					return err
+				}
+
+			}
+
+			return nil
+		}
+		return h
+	}
+	return m
+
+}
